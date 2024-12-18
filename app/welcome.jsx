277a81f24201ad885e9222f768782eb9c5d4179c
Matan <matan1775@gmail.com>
2024-12-18 19:18:31 +0200

    c1

--- app/welcome.jsx
+++ app/welcome.jsx
@@ -6,6 +6,7 @@ import { hp, wp } from '../constants/helpers/common'
 import { theme } from '../constants/theme'
 import Button from '../components/Button'
 
+
 const Welcome = () => {
   return (
     <ScreenWrapper bg="white">
@@ -36,7 +37,7 @@ const Welcome = () => {
                         Already have an account!
                     </Text>
                     <Pressable>
-                        <Text style={[styles.loginText, {color: theme.colors.primaryDark, fontWeight: theme.fonts.semibold}]}>
+                        <Text style={[styles.loginText, {color: theme.colors.primaryDark, fontWeight: theme.fonts.extraBold}]}>
                             Login
                         </Text>
                     </Pressable>
@@ -49,47 +50,5 @@ const Welcome = () => {
 
 export default Welcome
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        backgroundColor: 'white',
-        paddingHorizontal: wp(4)
 
-    },
-    welcomeImage: {
-        height: hp(50),
-        width: wp(100),
-        alignSelf: 'center'
-    },
-    title: {
-        color: theme.colors.text,
-        fontSize: hp(4),
-        textAlign: 'center',
-        fontWeight: theme.fonts.extraBold
-    },
-    punchline: {
-        textAlign: 'center',
-        paddingHorizontal: wp(10),
-        fontSize: hp(1.7),
-        color: theme.colors.text
-    },
-    footer: {
-        gap: 30,
-        width: '100%'
 
-    },
-    bottomTextContainer: {
-        flexDirection: 'row',
-        justifyContent: 'center',
-        alignItems: 'center',
-        gap: 5
-    },
-    loginText: {
-        textAlign: 'center',
-        color: theme.colors.text,
-        fontSize: hp(1.6)
-    }
-
-})
