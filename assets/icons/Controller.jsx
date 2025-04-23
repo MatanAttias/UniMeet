36826ec0bb22ba5368a0cb2a134771a4023b2d2f
Matan Atias <117354586+MatanAttias@users.noreply.github.com>
2025-04-24 01:10:09 +0300

    'upd'

---
+++ assets/icons/Controller.jsx
@@ -0,0 +1,13 @@
+import { HugeiconsIcon } from '@hugeicons/react-native';
+import { GameController03Icon } from '@hugeicons-pro/core-stroke-rounded';
+
+function MyComponent() {
+  return (
+    <HugeiconsIcon
+      icon={GameController03Icon}
+      size={24}
+      color="#000000"
+      strokeWidth={1.5}
+    />
+  );
+}
---
+++ assets/icons/Earth.jsx
@@ -0,0 +1,15 @@
+import { HugeiconsIcon } from '@hugeicons/react-native';
+import { EarthIcon } from '@hugeicons-pro/core-stroke-standard';
+
+function MyComponent() {
+  return (
+    <HugeiconsIcon
+      icon={EarthIcon}
+      size={24}
+      color="#000000"
+      strokeWidth={1.5}
+    />
+  );
+}
+
+
