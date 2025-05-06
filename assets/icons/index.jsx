b8362f715efd0efe75b8ef1f8b6d2926f6bfe8ea
MaorHurii <maorhu@ac.sce.ac.il>
2025-05-06 19:41:06 +0300

    .

--- assets/icons/index.jsx
+++ assets/icons/index.jsx
@@ -1,28 +1,28 @@
-import React from 'react'
-import Home from './Home'
-import { theme } from '../../constants/theme'
-import Mail from './Mail'
-import Lock from './Lock'
-import User from './User'
-import Heart from './Heart'
-import Plus from './Plus'
-import Search from './Search'
-import Location from './Location'
-import Call from './Call'
-import Camera from './Camera'
-import Edit from './Edit'
-import ArrowLeft from './ArrowLeft'
-import ThreeDotsCircle from './ThreeDotsCircle'
-import ThreeDotsHorizontal from './ThreeDotsHorizontal'
-import Comment from './Comment'
-import Share from './Share'
-import Send from './Send'
-import Delete from './Delete'
+import React from 'react';
+import Home from './Home';
+import { theme } from '../../constants/theme';
+import Mail from './Mail';
+import Lock from './Lock';
+import User from './User';
+import Heart from './Heart';
+import Plus from './Plus';
+import Search from './Search';
+import Location from './Location';
+import Call from './Call';
+import Camera from './Camera';
+import Edit from './Edit';
+import ArrowLeft from './ArrowLeft';
+import ThreeDotsCircle from './ThreeDotsCircle';
+import ThreeDotsHorizontal from './ThreeDotsHorizontal';
+import Comment from './Comment';
+import Share from './Share';
+import Send from './Send';
+import Delete from './Delete';
 import Logout from './Logout';
-import Image from './Image'
-import Video from './Video'
-import Confirm from './Confirm'
-import BookMark from './BookMark'
+import Image from './Image';
+import Video from './Video';
+import Confirm from './Confirm';
+import BookMark from './BookMark';
 
 const icons = {
   home: Home,
@@ -48,21 +48,25 @@ const icons = {
   video: Video,
   confirm: Confirm,
   bookmark: BookMark,
-}
+};
 
-const Icon = ({name, ...props}) => {
-    const IconComponent = icons[name];  
+const Icon = ({ name, size = 24, strokeWidth = 1.9, color, ...props }) => {
+  const IconComponent = icons[name];
+
+  if (!IconComponent) {
+    console.warn(`Icon "${name}" not found`);
+    return null;
+  }
+
   return (
     <IconComponent
-        height={props.size || 24}
-        width={props.size || 24}
-        strokeWidth={props.strokeWidth || 1.9}
-        color={theme.colors.textLight}
-        {...props}
-        
-    />    
-  )
-}
+      height={size}
+      width={size}
+      strokeWidth={strokeWidth}
+      color={color || theme.colors.textLight}
+      {...props}
+    />
+  );
+};
 
-export default Icon
-
+export default Icon;
